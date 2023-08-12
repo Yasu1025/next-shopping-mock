35c0a6fd6e9f30e1e87d6371cc869cf275ce4e36
Yasu1025 <sillyjp25@gmail.com>
2023-08-12 14:48:24 +0900

    [Setup] eslint prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
-import { createGlobalStyle } from 'styled-components'
-import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 // Global styles
 const GlobalStyle = createGlobalStyle`
